d62b90ebad35c5be91c6feb615bcd21d4bfff866
AbhinayMaurya <157687419+AbhinayMaurya@users.noreply.github.com>
2025-12-08 16:39:21 +0530

    Implement Watcher, Data Storage, and IPC Bridge

--- src/preload.cjs
+++ src/preload.cjs
@@ -1,6 +1,4 @@
 const { contextBridge, ipcRenderer } = require('electron');
-// CHANGE: Use 'import' instead of 'require'
-// import { contextBridge, ipcRenderer } from 'electron';
 
 // We expose a secure API to the Frontend (Renderer)
 contextBridge.exposeInMainWorld('electronAPI', {
