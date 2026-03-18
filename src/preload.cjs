9573ce186cc8473118b34df9569cbe29df56806b
AbhinayMaurya <157687419+AbhinayMaurya@users.noreply.github.com>
2026-03-18 18:08:46 +0530

    remove comment in preload

--- src/preload.cjs
+++ src/preload.cjs
@@ -5,9 +5,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   getUsageData: () => ipcRenderer.invoke('get-usage-data'),
   setCategory: (appName, category) => ipcRenderer.invoke('set-category', appName, category),
-  
-  // --- NEW FUNCTION ---
   setFocusMode: (state) => ipcRenderer.invoke('set-focus-mode', state),
-  // --- NEW FUNCTION ---
   clearData: () => ipcRenderer.invoke('clear-data') 
 });
